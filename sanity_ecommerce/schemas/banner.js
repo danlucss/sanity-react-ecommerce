aa2dd41ddb4c8a977e1118565cd948da18e18060
Daniel Lucas <daniellucas.bio@gmail.com>
2022-12-08 22:42:02 -0300

    add : banner schema

---
+++ sanity_ecommerce/schemas/banner.js
@@ -0,0 +1,62 @@
+export default {
+
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'buttonText',
+            title: 'Button Text',
+            type: 'string',
+        },
+
+        {
+            name: 'product',
+            title: 'Product',
+            type: 'string',
+        },
+        {
+            name: 'desc',
+            title: 'Desc',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'Small Text',
+            type: 'string',
+        },
+        {
+            name: 'midText',
+            title: 'Mid Text',
+            type: 'string',
+        },
+        {
+            name: 'bigText',
+            title: 'Big Text',
+            type: 'string',
+        },
+        {
+            name: 'largeText',
+            title: 'Large Text',
+            type: 'string',
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'Sale Time',
+            type: 'string',
+        },
+    ],
+}
